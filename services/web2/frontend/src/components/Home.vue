dc6d37ab0b75ee6f6c6b58e9f1a18cdc9c44fd6f
maltmann <michael.altmann@gmail.com>
2021-04-07 16:34:13 -0500

    Runs locally with Vue frontend and Flask backend

---
+++ services/web2/frontend/src/components/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <p>Home Page</p>
+    <p>Random number from backend: {{ randomNumber }}</p>
+    <button @click="getRandomFromBackend">New random number</button>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      randomNumber: 0
+    }
+  },
+  methods: {
+    getRandomInt(min, max) {
+      min = Math.ceil(min)
+      max = Math.floor(max)
+      return Math.floor(Math.random() * (max - min + 1)) + min
+    },
+    /* Just to be be able to mock out backend */
+    getRandom() {
+      this.randomNumber = this.getRandomInt(1, 100)
+    },
+    getRandomFromBackend() {
+      const path = `http://localhost:5000/api/random`
+      axios
+        .get(path)
+        .then(response => {
+          this.randomNumber = response.data.randomNumber
+        })
+        .catch(error => {
+          console.log(error)
+        })
+    }
+  },
+  created() {
+    this.getRandom()
+  }
+}
+</script>
